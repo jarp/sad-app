b1798ecde97c1fb403852a56a32306efcb6fd717
Jonathan A <jarp@nd.edu>
2025-04-14 10:30:37 -0400

    added constraints to migration

--- db/migrate/20250414142635_create_employees.rb
+++ db/migrate/20250414142635_create_employees.rb
@@ -1,11 +1,11 @@
 class CreateEmployees < ActiveRecord::Migration[7.2]
   def change
     create_table :employees do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false, index: true
       t.date :birthdate
-      t.boolean :is_active
-      t.boolean :is_manager
+      t.boolean :is_active, default: true
+      t.boolean :is_manager, default: false
       t.references :department, null: false, foreign_key: true
 
       t.timestamps
